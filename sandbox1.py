571e7188d9dc8aab26d13fcc43dc91be769f5f04
skipisu <schuyler.sampson@Nebraska.gov>
2016-10-05 07:53:01 -0500

    misc changes

--- sandbox1.py
+++ sandbox1.py
@@ -2,33 +2,27 @@
 
 import os
 
-def csvfile(filepath):
-	path, filename = os.path.split(filepath)
+def csvfile(user_filepath = "Input filepath directory:"):
+	while true:
+		filepath = input(user_filepath)
 	#print(path)
 	#print(filename)
-	for files in os.walk(path):
-		for file in files:
-			with open(filepath, 'r') as sitedata:
-				sitelines = sitedata.readlines()
-			#creates the filename 
-				for line in sitelines:
-					if "*Counter name   :" in line:
-						sitename = line[19:].strip()
-						
-					if "=DOCK TIME" in line:
-						filedate = line[28:].strip().replace("-", "").replace(" ", "_").replace(":", "")
-				filename = sitename + "_" + filedate
-				with open(filepath"/%s.TXT" % filename, 'w')\
-				as siteout:	
-					siteout.write("sitename, date, time, count\n")
-					
-					for line in sitelines:
+		for files in os.walk(filepath):
+			for file in files:
+				print(file)
+			#with open(filepath, 'r') as sitedata:
+			#	sitelines = sitedata.readlines()
 				
-						#extracts traffic counter name as 'sitename' 
-						if "*Counter name   :" in line:
-							sitename = line[19:].strip()
-
-						#combines 'sitename' and 'line' stripped to complete the file if line begins with a number
-						if line[0].isdigit():
-							alldata = sitename + ',' + line.strip()
-							siteout.write(alldata + '\n')
+				#creates the filename 
+			#	for line in sitelines:
+			#		if "*Counter name   :" in line:
+			#			sitename = line[19:].strip()
+			#			
+			#		if "=DOCK TIME" in line:
+			#	filename = sitename + "_" + filedate
+			#	print(filename)
+			#	with open(filepath % "/%s.TXT" % filename, 'w')as siteout:	
+			#		siteout.write("sitename, date, time, count\n")
+			#
+			#		for line in sitelines:
+			#		print(sitelines)
