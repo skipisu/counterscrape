c96dfc28f92678a19674d2b84b09a719ec12af5e
skipisu <schuyler.sampson@Nebraska.gov>
2016-10-07 07:40:24 -0500

    combine scrape with csv
    first attempt to combine the counter scarape master with the site to csv
    and rename as site file.

--- sandbox2.py
+++ sandbox2.py
@@ -9,14 +9,35 @@ basename, ext = os.path.splitext(filename)
 	
 with open(rawcounterfile, 'r')\
     as rawdata:
-    rawdata_lines = rawdata.readlines()	#creates list called datalines
-	#i = 1               # establishes starting point for naming new files 1 to n
-    #for line in rawdata_lines:
-    #    if line.strip():
-    #        test = rawdata_lines.append(line)
-	#		print(test)
-        #if line.strip() == "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~<<<<<<":
-        #    f_output = os.path.join(path, '{}_{}{}'.format(basename, i, ext))
+		rawdata_lines = rawdata.readlines()	#creates list called datalines
+		i = 1               # establishes starting point for naming new files 1 to n
+		for line in rawdata_lines:
+			if line.strip():
+				rawdata_lines.append(line)
+				
+			if line.strip() == "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~<<<<<<":
+				#creates the filename by site
+				for line in rawdata_lines:
+					if "*Counter name   :" in line:
+						sitename = line[19:].strip()
+					if "=DOCK TIME" in line:
+						filedate = line[28:].strip().replace("-", "").\
+						replace(" ", "_").replace(":", "")
+				filename = sitename + "_" + filedate
+				with open("C:/Users/schuyler.sampson/Documents/Sandbox/ShuttleOutput/%s.TXT" % filename, 'w')\
+				as siteout:	
+					siteout.write("sitename, date, time, count\n")
+					for line in rawdata_lines:
+		
+						#extracts traffic counter name as 'sitename' 
+						if "*Counter name   :" in line:
+							sitename = line[19:].strip()
+						
+						#combines 'sitename' and 'line' stripped to complete the file if line begins with a number
+						if line[0].isdigit():
+							alldata = sitename + ',' + line.strip()
+							siteout.write(alldata + '\n')
+        #   f_output = os.path.join(path, '{}_{}{}'.format(basename, i, ext))
 	    #	f_out = open(f_output, 'w')
         #    f_out.write(''.join(datalines))
         #    f_out.close()
