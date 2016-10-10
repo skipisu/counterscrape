9ef097ced297099f60257626c1c61c66078936a9
skipisu <schusamp@gmail.com>
2016-10-09 22:34:03 -0500

    still sand boxing

--- sandbox3.py
+++ sandbox3.py
@@ -1,14 +1,17 @@
+
+
 import os
 import csv
 
 
-rawcounterfile = "C:/Users/schuyler.sampson/Documents/Sandbox/ShuttleFile_VentureParks_20160908.TXT"
+rawcounterfile = "C:/Users/Schuyler/Documents/Sandbox/ShuttleFile_VentureParks_20160908.TXT"
 
 filepath, filename = os.path.split(rawcounterfile)
 basename, ext = os.path.splitext(filename)
 
 rawdata_lines = []
 sitedata_lines = []
+sitedata = {}
 i = 1	
 
 
@@ -20,45 +23,58 @@ i = 1
 
 #def read_file(rawcounterfile):
 with open(rawcounterfile, 'r') as rawdata:
-	next(rawdata)
-	for line in rawdata:
-		if line.strip():						#skip empty lines
-			rawdata_lines.append(line)	#creates list called datalines
-				
-		if line.strip() == "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~<<<<<<":
-			for line in rawdata_lines:
-				if "*Counter name   :" in line:
-					sitename = line[19:].strip()
+	rawdata_lines = rawdata.readlines()
+			
+	for line in rawdata_lines:
+		 if line.strip():
+			rawdata_lines.append(line)
+	print(rawdata_lines)
+		# if line.strip() == "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~<<<<<<":		
+			# ''.join(rawdata_lines)
+			# i += 1 
+			# sitedata_lines = []
+	
+		# sitedata_lines.append(line)
+			# i += 1
+			# sitedata_lines = []
+	# print(sitedata_lines)
+	
+#	sitedata_lines = ''.join.rawdata_lines
+#	print(sitedata_lines) 
+		#skip empty lines
+		#	rawdata_lines.append(line)	#creates list called datalines
+			
+			#sitedata_lines = rawdata_lines.write()
+		#print(sitedata_lines)
+		# if line.strip() == "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~<<<<<<":
+			# for line in rawdata_lines:
+				# if "*Counter name   :" in line:
+					# sitename = line[19:].strip()
 					
-				if "=DOCK TIME" in line:
-					filedate = "20" + line[28:-9].strip().replace("-", "")
-			site_filename = sitename + "_" + filedate
+				# if "=DOCK TIME" in line:
+					# filedate = "20" + line[28:-9].strip().replace("-", "")
+			# site_filename = sitename + "_" + filedate
 				
-			rawdata_lines.write("sitename, date, time, count\n")
+			#rawdata_lines.write("sitename, date, time, count\n")
 		
 				#extracts traffic counter name as 'sitename'  --- from counter to csv
-			if "*Counter name   :" in line:
-				sitename = line[19:].strip()
+			#if "*Counter name   :" in line:
+			#	sitename = line[19:].strip()
 
 			#combines 'sitename' and 'line' stripped to complete the file if line begins with a number
-			if line[0].isdigit():
-				sitedata = sitename + ',' + line.strip()
-				siteout.write(sitedata + '\n')
-			sitefile_output = os.path.join(filepath + '/ShuttleOutput/', '{}_{}{}'.format(site_filename, i, ext))
-			sitedata_out = open(sitefile_output, 'w')
-			sitedata_out.write(''.join(rawdata_lines))
-			sitedata_out.close()
-			i += 1
-			sitedata_lines = []
+			#if line[0].isdigit():
+			#	sitedata = sitename + ',' + line.strip()
+			#	siteout.write(sitedata + '\n')
+			#sitefile_output = os.path.join(filepath + '/ShuttleOutput/', '{}_{}{}'.format(site_filename, i, ext))
+			#sitedata_out = open(sitefile_output, 'w')
+			#sitedata_out.write(''.join(rawdata_lines))
+			#sitedata_out.close()
+			#i += 1
+			#sitedata_lines = []
 
 
 		
 
 #def main_newfile():
 #   userPath = input("Enter the New filefolder path:")
-#   path_exists(userPath)		
-		
-		
-		
-		
-		
+#   path_exists(userPath)	
