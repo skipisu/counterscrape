54b18fb4e0de06f69f48f5269809eda7e3c5794c
skipisu <schusamp@gmail.com>
2016-10-13 16:09:05 -0500

    minor adds - needs refined

--- streamline_sandbox.py
+++ streamline_sandbox.py
@@ -1,7 +1,10 @@
 
+
+#work file:	"C:/Users/schuyler.sampson/Documents/Sandbox/ShuttleFile_VentureParks_20160908.TXT"
+#home file:	"C:/Users/Schuyler/Documents/Sandbox/ShuttleFile_VentureParks_20160908.TXT"
 import os
 
-input_file = "C:/Users/Schuyler/Documents/Sandbox/ShuttleFile_VentureParks_20160908.TXT"
+input_file = "C:/Users/schuyler.sampson/Documents/Sandbox/ShuttleFile_VentureParks_20160908.TXT"
 file_data_buffer = []
 sitedata = []
 livedata = True
@@ -9,10 +12,16 @@ livedata = True
 
 with open(input_file, 'r') as alldata:
 	for line in alldata:
+		# if "*Counter name   :" in line:
+			# sitename = line[19:].strip()
 		if line.startswith("Calibration:"):
 			file_data_buffer = []
-		file_data_buffer.append(line)
-		if line.startswith("END OF DATA"):
-			for outline in file_data_buffer:
-				sitedata.append(outline)
-	print(sitedata)
+			
+		file_data_buffer.append(line.strip())
+	file_data_buffer.insert(1,"FUCKTHIS")
+	print(file_data_buffer)
+		# if line.startswith("END OF DATA"):
+			# for outline in file_data_buffer:
+				# sitedata.append(outline.strip())
+				
+# print(sitedata)
